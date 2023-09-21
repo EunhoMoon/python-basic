9620813ee6d0e1c32229380da6f75e3d21503a9f
Janek <eunho.moon@gmail.com>
2023-09-22 00:07:59 +0900

    변수

---
+++ myvenv/type/01.number,string,boolean.py
@@ -0,0 +1,17 @@
+# 숫자 자료형
+# 1. 정수형: 소수점이 없는 수
+print(1, 3, 0, -1, sep="/")
+
+# 2. 실수형: 소수점이 있는 수
+print(1.2, -1.1)
+
+# 문자 자료형
+# "" || ''
+print("Hello Python")
+print('Hello Python')
+print('"Hello Python" say "Hello"')
+
+# 불린 자료형
+# True || False
+print(True)
+print(False)
---
+++ myvenv/type/02.variable.py
@@ -0,0 +1,10 @@
+# 변수
+name = "Janek"
+age = 31
+job = "developer"
+print(name, age, job)
+
+# 변수의 값 변경
+age = 32
+job = "Engineer"
+print(name, age, job)
