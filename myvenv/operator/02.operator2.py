15b10785c7284572419f163b5c8243ab3edae382
Janek <eunho.moon@gmail.com>
2023-09-24 12:09:36 +0900

    비교, 논리, 멤버십 연산

---
+++ myvenv/operator/02.operator2.py
@@ -0,0 +1,18 @@
+# 비교 연산
+print(2 > 3)
+print(15 < 30)
+print(1.5 >= 0)
+print(3 <= 3)
+print("팙팗팚" == "팙팗팚")
+print("11111111111" != "111111111111")
+print("======================")
+
+# 논리 연산
+print(4 < 6 and 10 >= 10)   # True
+print(4 == 5 or 10 == 10)   # True
+print(not 5 == 5)           # False
+print("======================")
+
+# 멤버십 연산
+print("a" in "abc")     # True
+print("d" not in "abc") # True
