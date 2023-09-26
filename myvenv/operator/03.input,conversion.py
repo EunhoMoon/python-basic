5cfbee54c1b9b8665ed4044176fa12a87ae0af18
Janek <eunho.moon@gmail.com>
2023-09-26 15:28:58 +0900

    입력 및 형변환

--- myvenv/operator/03.input,conversion.py
+++ myvenv/operator/03.input,conversion.py
@@ -1,1 +1,13 @@
-x = input()
+# 입력
+x = input('첫번째 숫자를 입력하세요 >>> ')
+y = input('두번째 숫자를 입력하세요 >>> ')
+
+# 변환
+result = int(x) + int(y)
+
+print(result)
+
+# 실습문제
+birth = int(input('태어난 연도를 입력하세요 >>> '))
+age = 2023 - birth + 1
+print(f'당신의 나이는 {age}살 입니다.')
