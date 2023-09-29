7255768ffbb640b7fb00d1009559106c54a662e7
Janek <eunho.moon@gmail.com>
2023-09-29 17:40:02 +0900

    list

---
+++ myvenv/control-statement/03.iter.py
@@ -0,0 +1,17 @@
+champions = ['Liverpool', 'Chelsea', 'Arsenal', 'Manchester City', 'Manchester United']
+
+for champion in champions:
+    print(f'{champion} is a champion.')
+
+message = '파이팅'
+for word in message:
+    print(word)
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 5):
+    print(i)
+
+for i in range(1, 5, 2):
+    print(i)
