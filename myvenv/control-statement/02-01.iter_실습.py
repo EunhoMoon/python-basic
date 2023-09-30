b4492d8bba4228c657953e1926139fac51a5fa1d
Janek <eunho.moon@gmail.com>
2023-09-30 19:19:46 +0900

    iter 실습

---
+++ myvenv/control-statement/02-01.iter_실습.py
@@ -0,0 +1,13 @@
+number = int(input('몇 단을 출력할까요?: '))
+
+for i in range(1, 10):
+    print(f'{number} * {i} = {number * i}')
+
+words = ['안녕', '사자', '아침']
+
+is_answer = True
+while is_answer:
+    for word in words:
+        is_answer = word == input(f'{word}: ')
+        if not is_answer:
+            break
